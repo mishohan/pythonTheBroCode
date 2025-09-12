c0cc2663c04cc8a462d1898f2844e01a248baac3
mishohan <mishohan.o786@gmail.com>
2025-09-12 23:18:42 +0800

    09WightConversionProgram

---
+++ 09WeightConversion/main.py
@@ -0,0 +1,15 @@
+# Python weight conversion
+
+weight = float(input("Enter your weight: "))
+unit = input("kilograms or pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Your weight is: {round(weight, 2)} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"Your weight is: {round(weight, 2)} {unit}")
+else:
+    print(f"{unit} is not a valid unit")
