8763d5db25499e55d50672b44ddae3a51ee0d909
mishohan <mishohan.o786@gmail.com>
2025-09-12 16:10:38 +0800

    04 User Input()

---
+++ 04UserInput()/Exercise_01.py
@@ -0,0 +1,8 @@
+#Exercise 1: Rectangle Area Calc
+
+length = float(input("Enter the lenght: "))
+width = float(input("Enter the Width: "))
+
+area = length * width
+
+print(f"The Area of the Rectangle is {area} cm²")
