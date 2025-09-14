580eff0928465386fc01633c218d6bf247cc386e
mishohan <mishohan.o786@gmail.com>
2025-09-14 19:37:19 +0800

    20 Nested Loops

---
+++ 20NestedLoops/main.py
@@ -0,0 +1,19 @@
+# Nested Loop = A loop within another loop (outer, inner)
+#               Outer loop:
+#                   Inner loop:
+
+for x in range(3):
+    for y in range(1, 10):
+        print(y, end="")
+    print()
+
+
+# Example
+rows = int(input("Enter the # of rows: "))
+columns = int(input("Enter the # of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
