1685f9762b20ba8ef5f639fdea72a8951a01fca0
mishohan <mishohan.o786@gmail.com>
2025-09-19 11:23:51 +0800

    27 Random Number

---
+++ 27RandomNumber/main.py
@@ -0,0 +1,20 @@
+# Random Number
+
+import random
+
+# print(help(random))
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# number = random.randint(1, 20)
+number = random.randint(low, high)
+# number = random.random() # Returns random float between 0 to 1
+option = random.choice(options)
+random.shuffle(cards)
+
+print(number)
+print(option)
+print(cards)
